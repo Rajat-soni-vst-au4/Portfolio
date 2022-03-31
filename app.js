fb9f6ea0109c69347a0bbe5eae59992e78c50fcd
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-03-31 23:21:02 +0530

    https://github.com/Rajat-soni-vst-au4/Portfolio.git

---
+++ app.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls'); 
+const sectBtn = document.querySelectorAll('.control'); 
+const allSections = document.querySelector('.main-content');
+
+
+function PageTransitions(){
+    // buttonvlick active class
+
+    for(let i=0; i<sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            
+        })
+    }
+}
+
+
